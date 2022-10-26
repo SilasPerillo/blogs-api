d38b5ac9fe5fc79b04531f785746a98042569066
SilasPerillo <silasperillo@gmail.com>
2022-10-26 16:41:33 -0300

    req02 - criando model do User

---
+++ src/models/User.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+    tableName: 'users',
+    underscored: true,
+  });
+
+  return User;
+};
